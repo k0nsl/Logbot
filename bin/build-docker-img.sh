543251d5560d2414b7754e41971a72890ebd5ff2
root <k0nsl+git@k0nsl.org>
2017-05-28 20:43:22 -0400

    add shebang for scripts

--- bin/build-docker-img.sh
+++ bin/build-docker-img.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 docker build -t logbot .
 docker run -d -p 15000:15000 -e LOGBOT_NICK=genericnondescripguy -e LOGBOT_CHANNELS=#k0nsl,#trinity,#thehax -e LOGBOT_SERVER=ragnarok.k0nsl.org logbot
